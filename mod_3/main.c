6f38247c439253f6b8240ebc930242e2cb1998b8
Pabs <piort@me.com>
2020-06-16 10:54:14 +0200

    ningún cambio

--- mod_3/main.c
+++ mod_3/main.c
@@ -7,9 +7,29 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define size 10
+
+// At the beginning of main, or at least before you use rand()
+//srand(time(NULL));
+
+// Return array size
+#define getnum(x) (sizeof (x) / sizeof (*x))
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+
+
+    srand( (unsigned) time(NULL) * getpid()); // randomize seed
+    double Arr[size];
+    for(int i=0;i<size;i++){
+        Arr[i] = rand()%size;
+        printf("%d , %f \n",i,Arr[i]);
+    }
+    
+    
     return 0;
 }
